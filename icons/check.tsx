c2929d6ffb486806945872412e145d52792f520c
durosly <slyboydon1@gmail.com>
2024-12-26 16:52:13 +0100

    ✅feat: add new icons

---
+++ icons/check.tsx
@@ -0,0 +1,9 @@
+function IconCheck({ className }: { className?: string | undefined }) {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 12 10" fill="none">
+			<path d="M1 5.54545L3.72727 8.27273L11 1" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+		</svg>
+	);
+}
+
+export default IconCheck;
