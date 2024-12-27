c8d4184736d436327240eb12198dccd885de565b
durosly <slyboydon1@gmail.com>
2024-12-27 22:15:24 +0100

    📌fix: extract login form to own file

---
+++ app/(admin)/admin/_components/login-form.tsx
@@ -0,0 +1,23 @@
+function LoginForm() {
+	return (
+		<form className="card-body">
+			<div className="form-control">
+				<label className="label">
+					<span className="label-text">Email</span>
+				</label>
+				<input type="email" placeholder="email" className="input input-bordered" required />
+			</div>
+			<div className="form-control">
+				<label className="label">
+					<span className="label-text">Password</span>
+				</label>
+				<input type="password" placeholder="password" className="input input-bordered" required />
+			</div>
+			<div className="form-control mt-6">
+				<button className="btn btn-primary">Login</button>
+			</div>
+		</form>
+	);
+}
+
+export default LoginForm;
--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,3 +1,5 @@
+import LoginForm from "./_components/login-form";
+
 function AdminLoginPage() {
 	return (
 		<div className="hero bg-base-200 min-h-screen">
@@ -6,23 +8,7 @@ function AdminLoginPage() {
 					<h1 className="text-5xl font-bold">Login now!</h1>
 				</div>
 				<div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-					<form className="card-body">
-						<div className="form-control">
-							<label className="label">
-								<span className="label-text">Email</span>
-							</label>
-							<input type="email" placeholder="email" className="input input-bordered" required />
-						</div>
-						<div className="form-control">
-							<label className="label">
-								<span className="label-text">Password</span>
-							</label>
-							<input type="password" placeholder="password" className="input input-bordered" required />
-						</div>
-						<div className="form-control mt-6">
-							<button className="btn btn-primary">Login</button>
-						</div>
-					</form>
+					<LoginForm />
 				</div>
 			</div>
 		</div>
