00e1cfbeb0d7cba3c63651e4e8936693a9458d2e
durosly <slyboydon1@gmail.com>
2024-12-28 17:54:21 +0100

    ✅feat: add mongoose connect util function

---
+++ lib/connectMongo.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectMongo = async () => {
+	if (mongoose.connection.readyState >= 1) {
+		console.log("MongoDB is already connected.");
+		return mongoose.connection;
+	}
+
+	try {
+		const connection = await mongoose.connect(process.env.MONGODB_URL!);
+		console.log("MongoDB connected successfully.");
+		return connection;
+	} catch (error) {
+		console.error("MongoDB connection error:", error);
+		throw error;
+	}
+};
+
+export default connectMongo;
