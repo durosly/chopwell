d8bdc25a0e2e5691eaac315ea68001038f4d2a87
durosly <slyboydon1@gmail.com>
2024-12-29 04:29:00 +0100

    ✅feat: create google auth button

---
+++ app/(user)/(auth)/_components/google-button.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useMutation } from "@tanstack/react-query";
+import { signIn } from "next-auth/react";
+import { useRef } from "react";
+import { toast } from "sonner";
+
+function GoogleButton({ children }: { children: React.ReactNode }) {
+	const toastRef = useRef<string | number | undefined>(undefined);
+	const { isPending, mutate } = useMutation({
+		mutationFn: async () => {
+			toastRef.current = toast.loading("Authenticating user...", { duration: Infinity, id: toastRef.current });
+			await signIn("google", { redirectTo: "/" });
+		},
+		onError: (error) => {
+			toast.error("An error occured", { description: error.message, id: toastRef.current });
+		},
+		onSuccess: () => {
+			toast.info("Authenticating with google", { description: "you would be redirected shortly", id: toastRef.current });
+		},
+		onSettled: () => {
+			setTimeout(() => toast.dismiss(toastRef.current), 5000);
+		},
+	});
+
+	return (
+		<button disabled={isPending} onClick={() => mutate()} className="btn btn-block rounded-full border-accent">
+			{children}
+		</button>
+	);
+}
+
+export default GoogleButton;
