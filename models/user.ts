7a2283ba5b7366a412b0787252e96ab88e5a4f5b
durosly <slyboydon1@gmail.com>
2025-01-05 16:40:47 +0100

    📌update: add optional chaining to model request

--- models/user.ts
+++ models/user.ts
@@ -43,6 +43,6 @@ userSchema.pre("save", function (next) {
 	});
 });
 
-const UserModel = mongoose.models.User || mongoose.model("User", userSchema);
+const UserModel = mongoose.models?.User || mongoose.model("User", userSchema);
 
 export default UserModel;
