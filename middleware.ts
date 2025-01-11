6860524f50f1d7943ac7975a2837ee59040545f6
durosly <slyboydon1@gmail.com>
2025-01-11 02:19:42 +0100

    📌update: improve middleware to handle admin routes properly

--- middleware.ts
+++ middleware.ts
@@ -9,19 +9,16 @@ export default auth((req) => {
 	const isAdmin = req.auth?.user.is_admin;
 	// console.log(req.auth);
 
-	if (!pathname.startsWith("/dashboard") && isLoggedIn && isAdmin) {
+	if (!pathname.startsWith("/api") && !pathname.startsWith("/dashboard") && isLoggedIn && isAdmin) {
 		// TODO: check if this is an admin user
+		console.log("dashboaord redirect");
 		return Response.redirect(new URL("/dashboard", req.nextUrl));
 	}
 
 	if (pathname.startsWith("/dashboard") && !isAdmin) {
 		// redirect to landing page is user is not an admin
+		console.log("not dashboard");
 		return Response.redirect(new URL("/", req.nextUrl));
-	}
-
-	if (!req.auth && req.nextUrl.pathname !== "/login") {
-		// const newUrl = new URL("/login", req.nextUrl.origin);
-		// return Response.redirect(newUrl);
 	}
 
 	console.log("middleware fired...");
