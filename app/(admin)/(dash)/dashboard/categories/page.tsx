12b504460104808cde118b9fa43a2e98d0cf2db2
durosly <slyboydon1@gmail.com>
2025-01-11 02:15:37 +0100

    ✅feat(admin): display 'category add form' and category list display

---
+++ app/(admin)/(dash)/dashboard/categories/page.tsx
@@ -0,0 +1,14 @@
+import AddCategoryForm from "./_components/add-category-form";
+import CategoryDisplay from "./_components/category";
+
+function CategoryPage() {
+	return (
+		<>
+			<AddCategoryForm />
+
+			<CategoryDisplay />
+		</>
+	);
+}
+
+export default CategoryPage;
