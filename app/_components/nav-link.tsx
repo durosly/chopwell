212842baaaedec31b18366792bd71a24d66fdc47
durosly <slyboydon1@gmail.com>
2025-02-06 05:22:11 +0100

    📌refcator: use cn to handle classes

--- app/_components/nav-link.tsx
+++ app/_components/nav-link.tsx
@@ -3,6 +3,7 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { PropsWithChildren } from "react";
+import { cn } from "@/utils/cn";
 
 type PropType = {
 	path: string;
@@ -14,11 +15,12 @@ function NavLink({ path, children, className, activeClassName }: PropType) {
 	const pathname = usePathname();
 
 	return (
-		<nav>
-			<Link className={`${className} ${pathname === path ? activeClassName : ""}`} href={path}>
-				{children}
-			</Link>
-		</nav>
+		<Link
+			// className={`${className} ${pathname === path ? activeClassName : ""}`}
+			className={cn(className, pathname === path ? activeClassName : "")}
+			href={path}>
+			{children}
+		</Link>
 	);
 }
 
