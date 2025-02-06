922d37844fc22c435b53fe8034a6464703f07ac4
durosly <slyboydon1@gmail.com>
2025-02-06 05:19:41 +0100

    ✅feat(icons): add new icons

--- icons/user.tsx
+++ icons/user.tsx
@@ -1,6 +1,10 @@
 function IconUser({ className }: { className?: undefined | string }) {
 	return (
-		<svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 24" fill="none">
+		<svg
+			className={className}
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 25 24"
+			fill="none">
 			<path
 				d="M12.37 12.78C12.3 12.77 12.21 12.77 12.13 12.78C10.37 12.72 8.97 11.28 8.97 9.50998C8.97 7.69998 10.43 6.22998 12.25 6.22998C14.06 6.22998 15.53 7.69998 15.53 9.50998C15.52 11.28 14.13 12.72 12.37 12.78Z"
 				stroke="currentColor"
