fc5057e3509783cd243f0bfa2ce4547bf3c76819
durosly <slyboydon1@gmail.com>
2025-02-12 04:13:23 +0100

    📌refactor: improve search bar

--- app/(user)/(common)/_components/search-bar.tsx
+++ app/(user)/(common)/_components/search-bar.tsx
@@ -1,13 +1,78 @@
+"use client";
 import IconSearchNormal from "@/icons/search-normal";
+import { cn } from "@/utils/cn";
+import Form from "next/form";
+// import Link from "next/link";
+import { useRef } from "react";
 
-function SearchBar() {
+type PropType = {
+	showSubmitBtn?: boolean;
+	direction?: "row" | "column";
+	inputClassName?: string;
+	btnClassName?: string;
+	parentClassName?: string;
+};
+
+function SearchBar({
+	showSubmitBtn = false,
+	direction,
+	inputClassName,
+	btnClassName,
+	parentClassName,
+}: PropType) {
+	const inputRef = useRef<HTMLInputElement>(null);
 	return (
-		<form className="px-4 my-5">
-			<label className="input bg-neutral border-[0.5px] border-[#797373] flex items-center gap-2 rounded-full">
+		<Form
+			action={"/browse"}
+			className={cn(
+				"relative px-4 flex gap-2",
+				{ "flex-col": direction === "column" },
+				parentClassName
+			)}>
+			<label
+				className={cn(
+					"input relative bg-neutral border-[0.5px] border-[#797373] flex items-center gap-2 rounded-full",
+					inputClassName
+				)}>
 				<IconSearchNormal className="h-5 w-5 opacity-70" />
-				<input type="text" className="grow" placeholder="Search for product" />
+				<input
+					ref={inputRef}
+					type="search"
+					className="grow"
+					name="query"
+					placeholder="Search for product"
+					autoComplete="off"
+					required
+				/>
+				{/* suggestion dropdown */}
+				{/* <div className="absolute top-12 left-0 bg-base-100 w-full p-2 border rounded-md shadow-box z-50">
+					<ul>
+						<li>
+							<Link
+								className="block w-full px-3 py-3 hover:bg-black/5"
+								href="/browse">
+								Nice to mee you again and to check
+								if there is an overflow
+							</Link>
+						</li>
+						<li>
+							<Link
+								className="block w-full px-3 py-3 hover:bg-black/5"
+								href="/browse">
+								Nice
+							</Link>
+						</li>
+					</ul>
+				</div> */}
 			</label>
-		</form>
+			{showSubmitBtn && (
+				<button
+					type="submit"
+					className={cn("btn border-none btn-neutral", btnClassName)}>
+					Search
+				</button>
+			)}
+		</Form>
 	);
 }
 
