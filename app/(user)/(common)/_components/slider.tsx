3124eb489981163b14ef6acd5ef7ba5b6032f237
durosly <slyboydon1@gmail.com>
2025-02-12 04:15:20 +0100

    📌refactor: improve slider

--- app/(user)/(common)/_components/slider.tsx
+++ app/(user)/(common)/_components/slider.tsx
@@ -1,62 +1,74 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import Slider from "react-slick";
+
+const settings = {
+	dots: true,
+	infinite: true,
+	arrows: false,
+	speed: 500,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+
+	// className: "w-full h-[calc(100vh_-_200px)] aspect-video rounded-2xl overflow-hidden mb-4",
+	// appendDots: (dots: ReactNode) => (
+	// 	<div className="flex w-[80%] justify-center h-2 bg-[#DDDCDC] mx-auto rounded-full overflow-hidden">
+	// 		{dots}
+	// 	</div>
+	// ),
+
+	// customPaging: () => <a className="bg-dark flex-1">&nbsp;</a>,
+};
 
 function ImageSlider() {
 	return (
 		<>
 			<div className="px-5 mb-10">
-				<div className="carousel w-full aspect-video rounded-2xl mb-4">
-					<div id="item1" className="carousel-item w-full">
+				<Slider
+					{...settings}
+					// className="w-full h-[calc(100vh_-_200px)] aspect-video rounded-2xl overflow-hidden mb-4"
+				>
+					<div className="relative w-full h-[calc(100vh_-_200px)] rounded-2xl overflow-hidden">
 						<Image
 							src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-							width={300}
-							height={220}
-							className="w-full"
+							// width={300}
+							// height={220}
+							className="object-cover"
+							fill
 							alt=""
 						/>
 					</div>
-					<div id="item2" className="carousel-item w-full">
+					<div className="relative w-full h-[calc(100vh_-_200px)] rounded-2xl overflow-hidden">
 						<Image
 							src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-							width={300}
-							height={220}
+							// width={300}
+							// height={220}
+							className="object-cover"
+							fill
 							alt=""
-							className="w-full"
 						/>
 					</div>
-					<div id="item3" className="carousel-item w-full">
+					<div className="relative w-full h-[calc(100vh_-_200px)] rounded-2xl overflow-hidden">
 						<Image
 							src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-							width={300}
-							height={220}
+							// width={300}
+							// height={220}
+							className="object-cover"
+							fill
 							alt=""
-							className="w-full"
 						/>
 					</div>
-					<div id="item4" className="carousel-item w-full">
+					<div className="relative w-full h-[calc(100vh_-_200px)] rounded-2xl overflow-hidden">
 						<Image
 							src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-							width={300}
-							height={220}
-							className="w-full"
+							// width={300}
+							// height={220}
+							className="object-cover"
+							fill
 							alt=""
 						/>
 					</div>
-				</div>
-				<div className="flex w-[80%] justify-center h-2 bg-[#DDDCDC] mx-auto rounded-full overflow-hidden">
-					<a href="#item1" className="bg-dark flex-1">
-						&nbsp;
-					</a>
-					<a href="#item2" className="flex-1">
-						&nbsp;
-					</a>
-					<a href="#item3" className="flex-1">
-						&nbsp;
-					</a>
-					<a href="#item4" className="flex-1">
-						&nbsp;
-					</a>
-				</div>
+				</Slider>
 			</div>
 		</>
 	);
