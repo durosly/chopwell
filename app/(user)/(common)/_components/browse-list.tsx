4779ed16e446e8682a96ca02ccad18d82e455c25
durosly <slyboydon1@gmail.com>
2025-02-17 03:59:01 +0100

    ✅feat: modularize browse list

--- app/(user)/(common)/_components/browse-list.tsx
+++ app/(user)/(common)/_components/browse-list.tsx
@@ -1,310 +1,73 @@
 import IconHeart from "@/icons/heart";
 import IconStar from "@/icons/star";
+import { FoodDocument } from "@/models/food";
 import Image from "next/image";
+import LinkWithCartAndFav from "./link-with-cart-and-fav";
+import CartBtn from "./cart-btn";
+import { LuPlus } from "react-icons/lu";
+import FavBtn from "./fav-btn";
 
-function BrowseList() {
+function BrowseList({ items }: { items: FoodDocument[] }) {
 	return (
-		<div className="grid grid-cols-1 px-5 gap-2 py-3 w-full mb-10">
-			<div className="shadow-md border-[0.5px] flex-col rounded-2xl overflow-hidden">
-				<div className="relative h-[200px]">
-					<Image
-						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-						alt="Burger"
-						fill
-						className="object-cover"
-					/>
-					<div className="absolute top-0 w-full flex justify-between p-4">
-						<div></div>
-						<button className="btn btn-sm btn-square border-none bg-[#797373] text-secondary rounded-full p-1">
-							<IconHeart />
-						</button>
-					</div>
-				</div>
-				<div className="flex items-center justify-between p-2">
-					<div className="space-y-2">
-						<h3 className="font-bold">Hot Jollof Rice</h3>
-						<p className="text-xs">NG O Delivery fee</p>
-						<div className="flex gap-2 justify-between items-center text-xs font-bold">
-							<div className="flex gap-1 items-center">
-								<IconStar className="w-4 h-4 text-[#FFBB00]" />
-								<span>3.5</span>
-							</div>
-							<p>10-25 min</p>
+		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 px-5 gap-5 py-3 w-full mb-10">
+			{items.map((item) => (
+				<LinkWithCartAndFav
+					key={item._id as string}
+					href={`/product/${item._id}`}
+					className="shadow-md border-[0.5px] flex-col rounded-2xl overflow-hidden group">
+					<div className="relative h-[200px] overflow-hidden">
+						<Image
+							src={item.image}
+							alt={item.name}
+							fill
+							className="object-cover group-hover:scale-105 transition-transform duration-300"
+						/>
+						<div className="absolute top-0 w-full flex justify-between p-4">
+							<div></div>
+							<FavBtn className="btn btn-sm btn-square border-none bg-[#797373] text-secondary rounded-full p-1">
+								<IconHeart className="pointer-events-none" />
+							</FavBtn>
 						</div>
 					</div>
-					<p className="font-bold">NG 2,000</p>
-				</div>
-			</div>
-			<div className="shadow-md border-[0.5px] flex-col rounded-2xl overflow-hidden">
-				<div className="relative h-[200px]">
-					<Image
-						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-						alt="Burger"
-						fill
-						className="object-cover"
-					/>
-					<div className="absolute top-0 w-full flex justify-between p-4">
-						<div></div>
-						<button className="btn btn-sm btn-square border-none bg-[#797373] text-secondary rounded-full p-1">
-							<IconHeart />
-						</button>
-					</div>
-				</div>
-				<div className="flex items-center justify-between p-2">
-					<div className="space-y-2">
-						<h3 className="font-bold">Hot Jollof Rice</h3>
-						<p className="text-xs">NG O Delivery fee</p>
-						<div className="flex gap-2 justify-between items-center text-xs font-bold">
-							<div className="flex gap-1 items-center">
-								<IconStar className="w-4 h-4 text-[#FFBB00]" />
-								<span>3.5</span>
+					<div className="p-2">
+						<div className="space-y-2 mb-2">
+							<h3 className="font-bold">{item.name}</h3>
+							<div
+								className="tooltip tooltip-primary text-left"
+								data-tip={item.short_desc}>
+								<p className="text-xs line-clamp-1">
+									{item.short_desc}
+								</p>
 							</div>
-							<p>10-25 min</p>
-						</div>
-					</div>
-					<p className="font-bold">NG 2,000</p>
-				</div>
-			</div>
-			<div className="shadow-md border-[0.5px] flex-col rounded-2xl overflow-hidden">
-				<div className="relative h-[200px]">
-					<Image
-						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-						alt="Burger"
-						fill
-						className="object-cover"
-					/>
-					<div className="absolute top-0 w-full flex justify-between p-4">
-						<div></div>
-						<button className="btn btn-sm btn-square border-none bg-[#797373] text-secondary rounded-full p-1">
-							<IconHeart />
-						</button>
-					</div>
-				</div>
-				<div className="flex items-center justify-between p-2">
-					<div className="space-y-2">
-						<h3 className="font-bold">Hot Jollof Rice</h3>
-						<p className="text-xs">NG O Delivery fee</p>
-						<div className="flex gap-2 justify-between items-center text-xs font-bold">
-							<div className="flex gap-1 items-center">
-								<IconStar className="w-4 h-4 text-[#FFBB00]" />
-								<span>3.5</span>
+							<p className="font-bold whitespace-nowrap">
+								NG {item.price}
+							</p>
+							<div className="flex gap-2 justify-between items-center text-xs font-bold">
+								<div className="flex gap-1 items-center">
+									<IconStar className="w-4 h-4 text-[#FFBB00]" />
+									<span>
+										{
+											item.average_rating
+										}
+									</span>
+								</div>
+								<p>
+									{Math.round(
+										item.preparation_time -
+											item.preparation_time *
+												0.3
+									)}
+									-{item.preparation_time} min
+								</p>
 							</div>
-							<p>10-25 min</p>
 						</div>
+						<CartBtn className="btn btn-sm btn-block btn-primary">
+							<LuPlus />
+							Add to cart
+						</CartBtn>
 					</div>
-					<p className="font-bold">NG 2,000</p>
-				</div>
-			</div>
-			<div className="shadow-md border-[0.5px] flex-col rounded-2xl overflow-hidden">
-				<div className="relative h-[200px]">
-					<Image
-						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-						alt="Burger"
-						fill
-						className="object-cover"
-					/>
-					<div className="absolute top-0 w-full flex justify-between p-4">
-						<div></div>
-						<button className="btn btn-sm btn-square border-none bg-[#797373] text-secondary rounded-full p-1">
-							<IconHeart />
-						</button>
-					</div>
-				</div>
-				<div className="flex items-center justify-between p-2">
-					<div className="space-y-2">
-						<h3 className="font-bold">Hot Jollof Rice</h3>
-						<p className="text-xs">NG O Delivery fee</p>
-						<div className="flex gap-2 justify-between items-center text-xs font-bold">
-							<div className="flex gap-1 items-center">
-								<IconStar className="w-4 h-4 text-[#FFBB00]" />
-								<span>3.5</span>
-							</div>
-							<p>10-25 min</p>
-						</div>
-					</div>
-					<p className="font-bold">NG 2,000</p>
-				</div>
-			</div>
-			<div className="shadow-md border-[0.5px] flex-col rounded-2xl overflow-hidden">
-				<div className="relative h-[200px]">
-					<Image
-						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-						alt="Burger"
-						fill
-						className="object-cover"
-					/>
-					<div className="absolute top-0 w-full flex justify-between p-4">
-						<div></div>
-						<button className="btn btn-sm btn-square border-none bg-[#797373] text-secondary rounded-full p-1">
-							<IconHeart />
-						</button>
-					</div>
-				</div>
-				<div className="flex items-center justify-between p-2">
-					<div className="space-y-2">
-						<h3 className="font-bold">Hot Jollof Rice</h3>
-						<p className="text-xs">NG O Delivery fee</p>
-						<div className="flex gap-2 justify-between items-center text-xs font-bold">
-							<div className="flex gap-1 items-center">
-								<IconStar className="w-4 h-4 text-[#FFBB00]" />
-								<span>3.5</span>
-							</div>
-							<p>10-25 min</p>
-						</div>
-					</div>
-					<p className="font-bold">NG 2,000</p>
-				</div>
-			</div>
-			<div className="shadow-md border-[0.5px] flex-col rounded-2xl overflow-hidden">
-				<div className="relative h-[200px]">
-					<Image
-						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-						alt="Burger"
-						fill
-						className="object-cover"
-					/>
-					<div className="absolute top-0 w-full flex justify-between p-4">
-						<div></div>
-						<button className="btn btn-sm btn-square border-none bg-[#797373] text-secondary rounded-full p-1">
-							<IconHeart />
-						</button>
-					</div>
-				</div>
-				<div className="flex items-center justify-between p-2">
-					<div className="space-y-2">
-						<h3 className="font-bold">Hot Jollof Rice</h3>
-						<p className="text-xs">NG O Delivery fee</p>
-						<div className="flex gap-2 justify-between items-center text-xs font-bold">
-							<div className="flex gap-1 items-center">
-								<IconStar className="w-4 h-4 text-[#FFBB00]" />
-								<span>3.5</span>
-							</div>
-							<p>10-25 min</p>
-						</div>
-					</div>
-					<p className="font-bold">NG 2,000</p>
-				</div>
-			</div>
-			<div className="shadow-md border-[0.5px] flex-col rounded-2xl overflow-hidden">
-				<div className="relative h-[200px]">
-					<Image
-						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-						alt="Burger"
-						fill
-						className="object-cover"
-					/>
-					<div className="absolute top-0 w-full flex justify-between p-4">
-						<div></div>
-						<button className="btn btn-sm btn-square border-none bg-[#797373] text-secondary rounded-full p-1">
-							<IconHeart />
-						</button>
-					</div>
-				</div>
-				<div className="flex items-center justify-between p-2">
-					<div className="space-y-2">
-						<h3 className="font-bold">Hot Jollof Rice</h3>
-						<p className="text-xs">NG O Delivery fee</p>
-						<div className="flex gap-2 justify-between items-center text-xs font-bold">
-							<div className="flex gap-1 items-center">
-								<IconStar className="w-4 h-4 text-[#FFBB00]" />
-								<span>3.5</span>
-							</div>
-							<p>10-25 min</p>
-						</div>
-					</div>
-					<p className="font-bold">NG 2,000</p>
-				</div>
-			</div>
-			<div className="shadow-md border-[0.5px] flex-col rounded-2xl overflow-hidden">
-				<div className="relative h-[200px]">
-					<Image
-						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-						alt="Burger"
-						fill
-						className="object-cover"
-					/>
-					<div className="absolute top-0 w-full flex justify-between p-4">
-						<div></div>
-						<button className="btn btn-sm btn-square border-none bg-[#797373] text-secondary rounded-full p-1">
-							<IconHeart />
-						</button>
-					</div>
-				</div>
-				<div className="flex items-center justify-between p-2">
-					<div className="space-y-2">
-						<h3 className="font-bold">Hot Jollof Rice</h3>
-						<p className="text-xs">NG O Delivery fee</p>
-						<div className="flex gap-2 justify-between items-center text-xs font-bold">
-							<div className="flex gap-1 items-center">
-								<IconStar className="w-4 h-4 text-[#FFBB00]" />
-								<span>3.5</span>
-							</div>
-							<p>10-25 min</p>
-						</div>
-					</div>
-					<p className="font-bold">NG 2,000</p>
-				</div>
-			</div>
-			<div className="shadow-md border-[0.5px] flex-col rounded-2xl overflow-hidden">
-				<div className="relative h-[200px]">
-					<Image
-						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-						alt="Burger"
-						fill
-						className="object-cover"
-					/>
-					<div className="absolute top-0 w-full flex justify-between p-4">
-						<div></div>
-						<button className="btn btn-sm btn-square border-none bg-[#797373] text-secondary rounded-full p-1">
-							<IconHeart />
-						</button>
-					</div>
-				</div>
-				<div className="flex items-center justify-between p-2">
-					<div className="space-y-2">
-						<h3 className="font-bold">Hot Jollof Rice</h3>
-						<p className="text-xs">NG O Delivery fee</p>
-						<div className="flex gap-2 justify-between items-center text-xs font-bold">
-							<div className="flex gap-1 items-center">
-								<IconStar className="w-4 h-4 text-[#FFBB00]" />
-								<span>3.5</span>
-							</div>
-							<p>10-25 min</p>
-						</div>
-					</div>
-					<p className="font-bold">NG 2,000</p>
-				</div>
-			</div>
-			<div className="shadow-md border-[0.5px] flex-col rounded-2xl overflow-hidden">
-				<div className="relative h-[200px]">
-					<Image
-						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-						alt="Burger"
-						fill
-						className="object-cover"
-					/>
-					<div className="absolute top-0 w-full flex justify-between p-4">
-						<div></div>
-						<button className="btn btn-sm btn-square border-none bg-[#797373] text-secondary rounded-full p-1">
-							<IconHeart />
-						</button>
-					</div>
-				</div>
-				<div className="flex items-center justify-between p-2">
-					<div className="space-y-2">
-						<h3 className="font-bold">Hot Jollof Rice</h3>
-						<p className="text-xs">NG O Delivery fee</p>
-						<div className="flex gap-2 justify-between items-center text-xs font-bold">
-							<div className="flex gap-1 items-center">
-								<IconStar className="w-4 h-4 text-[#FFBB00]" />
-								<span>3.5</span>
-							</div>
-							<p>10-25 min</p>
-						</div>
-					</div>
-					<p className="font-bold">NG 2,000</p>
-				</div>
-			</div>
+				</LinkWithCartAndFav>
+			))}
 		</div>
 	);
 }
