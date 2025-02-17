c9a77af324c02a5294347ae83f371065d7701b0a
durosly <slyboydon1@gmail.com>
2025-02-17 03:27:34 +0100

    ✅feat: action to get food items

---
+++ actions/get-food-item.ts
@@ -0,0 +1,40 @@
+import connectMongo from "@/lib/connectMongo";
+import FoodModel from "@/models/food";
+
+type ParamType = {
+	limit?: number;
+	page?: number;
+	paginate?: boolean;
+	timeChoice?: "breakfast" | "lunch" | "dinner";
+	sortBy?: "price" | "average_rating";
+	order?: "asc" | "desc";
+};
+
+async function getFoodItems({ limit, page, paginate, timeChoice, sortBy, order }: ParamType) {
+	await connectMongo();
+
+	const query = timeChoice ? { timeChoice } : {};
+	if (!paginate) {
+		const foodItems = await FoodModel.find(query)
+			.sort({ [sortBy || "price"]: order || "asc" })
+			.limit(limit || 10);
+
+		if (!foodItems || !foodItems.length) return null;
+
+		return foodItems;
+	} else {
+		const options = {
+			page: page || 1,
+			limit: limit || 10,
+			sort: { [sortBy || "price"]: order || "asc" },
+		};
+
+		// const query = timeChoice ? { timeChoice } : {};
+
+		const foodItems = await FoodModel.paginate(query, options);
+
+		return foodItems;
+	}
+}
+
+export default getFoodItems;
