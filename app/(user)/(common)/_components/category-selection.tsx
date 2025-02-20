95739f05b3589e8d677cf44b9121823686c5cdce
durosly <slyboydon1@gmail.com>
2025-02-20 02:23:16 +0100

    ✅feat: display category from db

--- app/(user)/(common)/_components/category-selection.tsx
+++ app/(user)/(common)/_components/category-selection.tsx
@@ -1,21 +1,39 @@
+import getCategories from "@/actions/get-categories";
+import connectMongo from "@/lib/connectMongo";
+import { CategoryDocument } from "@/models/category";
 import Image from "next/image";
 import Link from "next/link";
 
-function CategorySelection() {
+async function CategorySelection() {
+	await connectMongo();
+	const categories = await getCategories({ limit: 6 });
+
+	if (!categories || !categories.length) return null;
+
 	return (
-		<div className="carousel carousel-center items-start px-5 gap-4 w-full mb-10">
-			<Link href="/" className="carousel-item flex-col items-center">
-				<div className="relative aspect-square w-[80px] rounded-full overflow-hidden">
-					<Image
-						fill
-						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-						alt="Pizza"
-					/>
-				</div>
-				<p className="font-bold mt-2">Pizza</p>
-			</Link>
-			<Link href="/" className="carousel-item flex-col items-center">
-				<div className="relative aspect-square w-[80px] rounded-full overflow-hidden">
+		<div className="carousel carousel-center md:justify-center items-start px-5 gap-4 w-full mb-10">
+			{Array.isArray(categories) &&
+				categories.map((category: CategoryDocument) => (
+					<Link
+						key={category._id as string}
+						href={`/browse?category=${category._id}`}
+						className="carousel-item flex-col items-center">
+						<div className="relative aspect-square w-[80px] md:w-[120px] rounded-full md:rounded-xl overflow-hidden">
+							<Image
+								fill
+								src={category.cover_image}
+								placeholder="blur"
+								blurDataURL={
+									category.coverImagePlaceholder
+								}
+								alt="Pizza"
+							/>
+						</div>
+						<p className="font-bold mt-2">{category.name}</p>
+					</Link>
+				))}
+			{/* <Link href="/" className="carousel-item flex-col items-center">
+				<div className="relative aspect-square w-[80px] md:w-[120px] rounded-full md:rounded-xl overflow-hidden">
 					<Image
 						fill
 						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
@@ -25,7 +43,7 @@ function CategorySelection() {
 				<p className="font-bold mt-2">Semo</p>
 			</Link>
 			<Link href="/" className="carousel-item flex-col items-center">
-				<div className="relative aspect-square w-[80px] rounded-full overflow-hidden">
+				<div className="relative aspect-square w-[80px] md:w-[120px] rounded-full md:rounded-xl overflow-hidden">
 					<Image
 						fill
 						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
@@ -35,7 +53,7 @@ function CategorySelection() {
 				<p className="font-bold mt-2">Rice</p>
 			</Link>
 			<Link href="/" className="carousel-item flex-col items-center">
-				<div className="relative aspect-square w-[80px] rounded-full overflow-hidden">
+				<div className="relative aspect-square w-[80px] md:w-[120px] rounded-full md:rounded-xl overflow-hidden">
 					<Image
 						fill
 						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
@@ -45,7 +63,7 @@ function CategorySelection() {
 				<p className="font-bold mt-2">Fried Rice</p>
 			</Link>
 			<Link href="/" className="carousel-item flex-col items-center">
-				<div className="relative aspect-square w-[80px] rounded-full overflow-hidden">
+				<div className="relative aspect-square w-[80px] md:w-[120px] rounded-full md:rounded-xl overflow-hidden">
 					<Image
 						fill
 						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
@@ -55,7 +73,7 @@ function CategorySelection() {
 				<p className="font-bold mt-2">Garri</p>
 			</Link>
 			<Link href="/" className="carousel-item flex-col items-center">
-				<div className="relative aspect-square w-[80px] rounded-full overflow-hidden">
+				<div className="relative aspect-square w-[80px] md:w-[120px] rounded-full md:rounded-xl overflow-hidden">
 					<Image
 						fill
 						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
@@ -65,7 +83,7 @@ function CategorySelection() {
 				<p className="font-bold mt-2">Salad</p>
 			</Link>
 			<Link href="/" className="carousel-item flex-col items-center">
-				<div className="relative aspect-square w-[80px] rounded-full overflow-hidden">
+				<div className="relative aspect-square w-[80px] md:w-[120px] rounded-full md:rounded-xl overflow-hidden">
 					<Image
 						fill
 						src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
@@ -73,7 +91,7 @@ function CategorySelection() {
 					/>
 				</div>
 				<p className="font-bold mt-2">Chicken</p>
-			</Link>
+			</Link> */}
 		</div>
 	);
 }
