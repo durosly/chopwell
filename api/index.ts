64a16d3612589372a797197c902a083952e10a24
durosly <slyboydon1@gmail.com>
2025-02-20 01:03:35 +0100

    ✅feat: handle favourite

--- api/index.ts
+++ api/index.ts
@@ -4,3 +4,30 @@ export async function getCategories(page: number, query: string, signal: AbortSi
 	const response = await axios("/api/admin/categories", { params: { page, query }, signal });
 	return response.data;
 }
+
+// Favoutite API
+export async function addItemToFavourite(
+	{ foodId, sessionId }: { foodId: string; sessionId: string },
+	signal?: AbortSignal
+) {
+	const response = await axios.post("/api/auth/fav", { foodId, sessionId }, { signal });
+	return response.data;
+}
+export async function removeItemFromFavourite(
+	{ foodId, sessionId }: { foodId: string; sessionId: string },
+	signal?: AbortSignal
+) {
+	const response = await axios.delete("/api/auth/fav", {
+		data: { foodId, sessionId },
+		signal,
+	});
+	return response.data;
+}
+
+export async function getFavourites(
+	{ sessionId, full }: { sessionId: string; full?: boolean },
+	signal?: AbortSignal
+) {
+	const response = await axios("/api/auth/fav", { params: { sessionId, full }, signal });
+	return response.data;
+}
