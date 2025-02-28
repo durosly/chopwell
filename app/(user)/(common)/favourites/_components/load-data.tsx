ba6d6246c483228dcff61bb9cce3a562c61062d2
durosly <slyboydon1@gmail.com>
2025-02-28 22:41:42 +0100

    📌refactor: remove session from local storage

--- app/(user)/(common)/favourites/_components/load-data.tsx
+++ app/(user)/(common)/favourites/_components/load-data.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import { getFavourites } from "@/api";
-import getAnonymousSessionId from "@/utils/get-anonymous-session-id";
 import { useQuery } from "@tanstack/react-query";
 import NoFavourites from "./no-favourites";
 import BrowseList from "../../_components/browse-list";
 
 function LoadData() {
-	const anonymousSession = getAnonymousSessionId();
 	const { data: favourites, isLoading } = useQuery({
 		queryKey: ["favourites", { full: true }],
-		queryFn: () => getFavourites({ sessionId: anonymousSession, full: true }),
+		queryFn: () => getFavourites({ full: true }),
 	});
 
 	if (isLoading) return <div>Loading...</div>;
