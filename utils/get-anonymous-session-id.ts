90d18c59a412ee6b135b106d550d0d0cbc278e01
durosly <slyboydon1@gmail.com>
2025-03-03 02:00:50 +0100

    📌refactor: improve session handling
    Only create new anonymous session when required

--- utils/get-anonymous-session-id.ts
+++ utils/get-anonymous-session-id.ts
@@ -1,14 +1,15 @@
 "use server";
+
 import "server-only";
 import { v4 as uuidv4 } from "uuid";
 import { cookies } from "next/headers";
 
-async function getAnonymousSessionId() {
+async function getAnonymousSessionId({ get }: { get?: boolean } = { get: true }) {
 	const anonymousSessionName = "sessionId";
 	const cookieStore = await cookies();
 
 	let sessionId = cookieStore.get(anonymousSessionName)?.value;
-	if (!sessionId) {
+	if (!sessionId && !get) {
 		sessionId = uuidv4();
 		cookieStore.set({
 			name: anonymousSessionName,
@@ -17,6 +18,8 @@ async function getAnonymousSessionId() {
 			path: "/",
 			secure: process.env.NODE_ENV === "production",
 		});
+	} else {
+		sessionId = "";
 	}
 
 	return sessionId;
