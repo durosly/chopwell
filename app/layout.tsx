d953455efb76ce11db2995ef8d62a15e012c0ac6
durosly <slyboydon1@gmail.com>
2025-03-03 01:58:53 +0100

    ✔️chore: update tailwindcss and daisyUI to latest
    - tailwindcss v4
    - daisyui v5

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${inter.variable} antialiased bg-gray-100`}>
+			<body className={`${inter.variable} antialiased`}>
 				<QueryWrapper>
 					{children}
 					<ToastWrapper />
