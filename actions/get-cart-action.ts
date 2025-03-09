cf78adacaed24a58f3d9e8b853b9488fe3696298
durosly <slyboydon1@gmail.com>
2025-03-09 23:32:30 +0100

    📌refactor: load cart data with separated cart components

--- actions/get-cart-action.ts
+++ actions/get-cart-action.ts
@@ -6,6 +6,8 @@ import CartModel from "@/models/cart";
 import FoodModel from "@/models/food";
 import PromoModel from "@/models/promo";
 import getAnonymousSessionId from "@/utils/get-anonymous-session-id";
+import CartItemGroupModel from "@/models/cart-item-group";
+import CartItemModel from "@/models/cart-item";
 
 async function getCartDataAction() {
 	await connectMongo();
@@ -33,70 +35,70 @@ async function getCartDataAction() {
 		data: [],
 	};
 
-	const subtotal = await cart.group.reduce(
-		async (accPromise: Promise<number>, group: { items: any[]; title: string }) => {
-			const acc = await accPromise;
+	const cartGroup = await CartItemGroupModel.find({ cartId: cart._id }).sort({
+		createdAt: -1,
+	});
 
-			cartData.data.push({
-				_id: group._id,
-				title: group.title,
-				total: 0,
-				percentage: 0,
-				items: [],
+	if (cartGroup.length === 0) {
+		return { message: "Cart is empty", status: false };
+	}
+
+	for (const group of cartGroup) {
+		const groupData = {
+			_id: group._id,
+			title: group.title,
+			items: [],
+			total: 0,
+			percentage: 0,
+		};
+
+		const cartItems = await CartItemModel.find({ groupId: group._id }).sort({
+			createdAt: -1,
+		});
+
+		for (const item of cartItems) {
+			const food = await FoodModel.findById(item.foodId).populate("_categoryIds");
+			if (!food) continue;
+
+			const price = food.price;
+			const promo = await PromoModel.findOne({
+				items: { $elemMatch: { foodId: food._id } },
+				startDate: { $lte: now },
+				endDate: { $gte: now },
 			});
 
-			const total = await group.items.reduce(
-				async (
-					accPromise: Promise<number>,
-					item: { foodId: string; quantity: number }
-				) => {
-					const acc = await accPromise;
-					const food = await FoodModel.findById(item.foodId).populate(
-						"_categoryIds"
-					);
-					if (!food) return acc;
-					const price = food.price;
-					const promo = await PromoModel.findOne({
-						items: { $elemMatch: { foodId: food._id } },
-						startDate: { $lte: now },
-						endDate: { $gte: now },
-					});
+			const discount = promo?.precentageDiscount || 0;
+			const discountedPrice = price - (price * discount) / 100;
 
-					// console.log("promo", promo);
+			// @ts-expect-error: not specified types
+			groupData.items.push({
+				_id: item._id,
+				quantity: item.quantity,
+				price: discountedPrice,
+				actualPrice: price,
+				name: food.name,
+				available: food.available,
+				image: food.image,
+				preparation_time: food.preparation_time,
+				promo: promo,
+				category: food._categoryIds
+					// @ts-expect-error: not specified types
+					.map((category) => category.name)
+					.join(", "),
+			});
 
-					// if promo apply discount else 0
+			groupData.total += discountedPrice * item.quantity;
+		}
 
-					const discount = promo?.precentageDiscount || 0;
-					const discountedPrice = price - (price * discount) / 100;
-
-					cartData.data[cartData.data.length - 1].items.push({
-						_id: food._id,
-						quantity: item.quantity,
-						price: discountedPrice,
-						name: food.name,
-						available: food.available,
-						image: food.image,
-						preparation_time: food.preparation_time,
-						promo: promo,
-					});
-
-					return acc + discountedPrice * item.quantity;
-				},
-				Promise.resolve(0)
-			);
-
-			cartData.data[cartData.data.length - 1].total = total;
-
-			return acc + total;
-		},
-		Promise.resolve(0)
-	);
-
-	cartData.subtotal = subtotal;
+		// @ts-expect-error: not specified types
+		cartData.data.push(groupData);
+		cartData.subtotal += groupData.total;
+	}
 
 	// calculate percentage of each group
 	cartData.data.forEach((group) => {
-		group.percentage = (group.total / subtotal) * 100;
+		// @ts-expect-error: not specified types
+		group.percentage = (group.total / cartData.subtotal) * 100;
 	});
 
 	return { status: true, data: cartData };
