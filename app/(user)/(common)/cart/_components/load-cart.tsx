a13ea0ef53d0ae087370b557e3c14f915a319f3f
durosly <slyboydon1@gmail.com>
2025-03-10 02:57:54 +0100

    ✅feat: extract action buttons from component

--- app/(user)/(common)/cart/_components/load-cart.tsx
+++ app/(user)/(common)/cart/_components/load-cart.tsx
@@ -1,17 +1,19 @@
 "use client";
-import IconTrash from "@/icons/trash";
+import { getCart } from "@/api";
 import { handleError } from "@/lib/handleError";
 import commaNumber from "@/utils/comma-number";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
-import { LuBadgeAlert, LuMinus, LuPencil, LuPlus } from "react-icons/lu";
-import { getCart } from "@/api";
-import LoadingCartAnimation from "./loading-cart";
-import GroupTitle from "./group-title";
-import AddNewCartGroupButton from "./add-new-cart-group-btn";
 import { Fragment } from "react";
+import { LuBadgeAlert } from "react-icons/lu";
+import AddNewCartGroupButton from "./add-new-cart-group-btn";
 import GroupDeleteBtn from "./group-delete-btn";
+import GroupTitle from "./group-title";
+import IncreaseItemQuantity from "./increase-item-quantity";
+import LoadingCartAnimation from "./loading-cart";
+import ReduceItemQuantity from "./reduce-item-quantity";
+import RemoveCartItemBtn from "./remove-cart-item-btn";
 
 function LoaCart() {
 	const { data, isLoading, isError, error } = useQuery({
@@ -40,84 +42,138 @@ function LoaCart() {
 
 	return (
 		<>
-			{data.data.map((group, i: number) => (
-				<Fragment key={group._id}>
-					<div className="pl-5 max-md:pr-5 max-lg:pr-10 md:pl-10">
-						<hr className="mb-5" />
-					</div>
-					<div className="px-5 md:px-10 mb-10 last:mb-0">
-						<div>
-							<div className="mb-5">
-								<div className="flex justify-between items-center gap-2">
-									<div className="flex items-center gap-2">
-										<h3 className="text-xl font-bold">
-											{
-												group.title
-											}
-										</h3>
-										<GroupTitle
-											groupId={
-												group._id
-											}
-										/>
-										<GroupDeleteBtn
-											groupId={
-												group._id
-											}
-											title={
-												group.title
-											}
-										/>
+			{
+				// @ts-expect-error: specify type of group
+				data.data.map((group, i: number) => (
+					<Fragment key={group._id}>
+						<div className="pl-5 max-md:pr-5 max-lg:pr-10 md:pl-10">
+							<hr className="mb-5" />
+						</div>
+						<div className="px-5 md:px-10 mb-10 last:mb-0">
+							<div>
+								<div className="mb-5">
+									<div className="flex justify-between items-center gap-2">
+										<div className="flex items-center gap-2">
+											<h3 className="text-xl font-bold">
+												{
+													group.title
+												}
+											</h3>
+											<GroupTitle
+												groupId={
+													group._id
+												}
+											/>
+											<GroupDeleteBtn
+												groupId={
+													group._id
+												}
+												title={
+													group.title
+												}
+											/>
+										</div>
+										{i === 0 && (
+											<AddNewCartGroupButton />
+										)}
 									</div>
-									{i === 0 && (
-										<AddNewCartGroupButton />
-									)}
+									<p className="text-xs">
+										Total:{" "}
+										{commaNumber(
+											group.total
+										)}
+										<span className="text-gray-500">
+											(
+											{Math.round(
+												group.percentage
+											)}
+											%)
+										</span>
+									</p>
 								</div>
-								<p className="text-xs">
-									Total:{" "}
-									{commaNumber(group.total)}
-									<span className="text-gray-500">
-										({group.percentage}
-										%)
-									</span>
-								</p>
-							</div>
-							<ul className="grid grid-cols-1 gap-5">
-								{group.items.map(
-									(cartItem, i: number) => (
-										<li
-											key={
-												cartItem._id
-											}
-											className="border-b pb-5 last:border-b-0 last:pb-0">
-											<Link
-												href={`/product/${cartItem._id}`}
-												className="flex gap-5 items-start ">
-												<div className="relative w-[70px] flex-none aspect-square rounded-md overflow-hidden">
-													<Image
-														src={
-															cartItem.image
-														}
-														alt={
-															cartItem.name
-														}
-														fill
-														className="object-cover"
-													/>
-												</div>
-												<div className="flex justify-between flex-1 mb-2">
-													<div>
-														<h3 className="text-xl font-bold">
-															{
+								<ul className="grid grid-cols-1 gap-5">
+									{group.items.map(
+										// @ts-expect-error: specify type of cartItem
+										(cartItem) => (
+											<li
+												key={
+													cartItem._id
+												}
+												className="border-b pb-5 last:border-b-0 last:pb-0">
+												<Link
+													href={`/product/${cartItem._id}`}
+													className="flex gap-5 items-start ">
+													<div className="relative w-[70px] flex-none aspect-square rounded-md overflow-hidden">
+														<Image
+															src={
+																cartItem.image
+															}
+															alt={
 																cartItem.name
 															}
-														</h3>
-														<div className="sm:hidden">
+															fill
+															className="object-cover"
+														/>
+													</div>
+													<div className="flex justify-between flex-1 mb-2">
+														<div>
+															<h3 className="text-xl font-bold">
+																{
+																	cartItem.name
+																}
+															</h3>
+															<div className="sm:hidden">
+																<p className="font-bold text-xl">
+																	{commaNumber(
+																		cartItem.price
+																	)}
+																</p>
+																{!!cartItem.promo && (
+																	<div className="flex items-center gap-1">
+																		<p className="line-through text-gray-400">
+																			NG
+																			1,300
+																		</p>
+																		<span className="badge badge-accent">
+																			-40%
+																		</span>
+																	</div>
+																)}
+															</div>
+															<ul className="text-[10px] text-[#3A3939]">
+																<li>
+																	In
+																	stock
+																</li>
+																<li>
+																	food
+																</li>
+															</ul>
+														</div>
+														{!!cartItem.promo && (
+															<div className="hidden sm:flex items-center gap-1">
+																<p className="font-bold text-xl">
+																	NG
+																	2,000
+																</p>
+																<div className="flex items-center gap-1">
+																	<p className="line-through text-gray-400">
+																		NG
+																		1,300
+																	</p>
+																	<span className="badge badge-accent">
+																		-40%
+																	</span>
+																</div>
+															</div>
+														)}
+														<div className="max-sm:hidden">
 															<p className="font-bold text-xl">
 																{commaNumber(
 																	cartItem.price
 																)}
 															</p>
+
 															{!!cartItem.promo && (
 																<div className="flex items-center gap-1">
 																	<p className="line-through text-gray-400">
@@ -130,91 +186,50 @@ function LoaCart() {
 																</div>
 															)}
 														</div>
-														<ul className="text-[10px] text-[#3A3939]">
-															<li>
-																In
-																stock
-															</li>
-															<li>
-																food
-															</li>
-														</ul>
 													</div>
-													{!!cartItem.promo && (
-														<div className="hidden sm:flex items-center gap-1">
-															<p className="font-bold text-xl">
-																NG
-																2,000
-															</p>
-															<div className="flex items-center gap-1">
-																<p className="line-through text-gray-400">
-																	NG
-																	1,300
-																</p>
-																<span className="badge badge-accent">
-																	-40%
-																</span>
-															</div>
+												</Link>
+
+												<div className="flex-1">
+													<div className="flex justify-between items-center gap-5">
+														<RemoveCartItemBtn
+															cartItemId={
+																cartItem._id
+															}
+														/>
+														<div className="flex items-center gap-5">
+															<ReduceItemQuantity
+																itemId={
+																	cartItem._id
+																}
+																quantity={
+																	cartItem.quantity
+																}
+															/>
+															<span className="">
+																{
+																	cartItem.quantity
+																}
+															</span>
+															<IncreaseItemQuantity
+																itemId={
+																	cartItem._id
+																}
+																quantity={
+																	cartItem.quantity
+																}
+															/>
 														</div>
-													)}
-													<div className="max-sm:hidden">
-														<p className="font-bold text-xl">
-															NG
-															2,000
-														</p>
-
-														{!!cartItem.promo && (
-															<div className="flex items-center gap-1">
-																<p className="line-through text-gray-400">
-																	NG
-																	1,300
-																</p>
-																<span className="badge badge-accent">
-																	-40%
-																</span>
-															</div>
-														)}
 													</div>
 												</div>
-											</Link>
-
-											<div className="flex-1">
-												{/* <form action="">
-                            <label className="form-control">
-                                <textarea
-                                    className="textarea textarea-sm textarea-bordered border-[#C2C2C2] h-10"
-                                    placeholder="Add special request here..."></textarea>
-                            </label>
-                        </form> */}
-
-												<div className="flex justify-between items-center gap-5">
-													<button className="btn btn-sm btn-accent">
-														<IconTrash className="w-5 h-5" />{" "}
-														Remove
-													</button>
-													<div className="">
-														<button
-															disabled
-															className="btn btn-square btn-sm">
-															<LuMinus />
-														</button>
-														<span className="btn btn-ghost btn-square">
-															1
-														</span>
-														<button className="btn btn-square btn-sm">
-															<LuPlus />
-														</button>
-													</div>
-												</div>
-											</div>
-										</li>
-									)
-								)}
-							</ul>
+											</li>
+										)
+									)}
+								</ul>
+							</div>
 						</div>
-					</div>
-				</Fragment>
-			))}
+					</Fragment>
+				))
+			}
 		</>
 	);
 }
