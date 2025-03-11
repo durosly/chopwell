c27c7c5e16a5ae19608bdd8f8a4633890e19bc74
durosly <slyboydon1@gmail.com>
2025-03-11 01:38:34 +0100

    ✅feat: create cart summary component

---
+++ app/(user)/(common)/cart/_components/cart-summary.tsx
@@ -0,0 +1,26 @@
+import SubtotalDisplay from "./subtotal-display";
+import { LuBadgeInfo } from "react-icons/lu";
+import Link from "next/link";
+
+function CartSummary() {
+	return (
+		<>
+			<h2 className="text-xl font-semibold uppercase mb-4">Cart Summary</h2>
+			<div className="flex justify-between gap-2">
+				<p>Subtotal</p>
+				<p className="text-xl font-bold">
+					<SubtotalDisplay />
+				</p>
+			</div>
+			<p className="text-xs flex item-center gap-1 text-gray-500 mb-4">
+				<LuBadgeInfo /> Delivery fee not included yet
+			</p>
+			<Link href={"/checkout"} className="btn btn-primary btn-block">
+				Checkout (
+				<SubtotalDisplay />)
+			</Link>
+		</>
+	);
+}
+
+export default CartSummary;
