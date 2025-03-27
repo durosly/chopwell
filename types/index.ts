fbd6119c59bf0825f9bdaafa51a588fed778a104
durosly <slyboydon1@gmail.com>
2025-03-27 22:47:01 +0100

    ✅feat: add formatted address type

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,10 @@
 export type UserType = "admin" | "employee" | "customer";
+
+export type FormattedAddress = {
+	_id: string;
+	landmark: string;
+	location: string;
+	region: string;
+	deliveryPrice: number;
+	title: string;
+};
