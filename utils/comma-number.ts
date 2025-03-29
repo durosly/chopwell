a0caa0bae6ba3213822f7ef9d6168c4a34855aa2
durosly <slyboydon1@gmail.com>
2025-03-29 20:12:21 +0100

    📌refactor: update condition to work on "0" value

--- utils/comma-number.ts
+++ utils/comma-number.ts
@@ -1,8 +1,5 @@
-function commaNumber(
-	number: number | string,
-	params?: { currency?: "NGN" | "EUR"; style?: "currency" | "decimal" }
-): string {
-	if (!Number(number)) return number.toString();
+function commaNumber(number: number | string, params?: { currency?: "NGN" | "EUR"; style?: "currency" | "decimal" }): string {
+	if (typeof Number(number) !== "number") return number.toString();
 
 	const { currency = "NGN", style = "currency" } = params || {};
 	return new Intl.NumberFormat("en-NG", {
