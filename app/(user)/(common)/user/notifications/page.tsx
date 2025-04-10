b8051ce3f7db1b05c5752e167703b0685f909a66
durosly <slyboydon1@gmail.com>
2025-04-11 00:14:57 +0100

    ✅feat: enhance notifications page with data prefetching and hydration support

--- app/(user)/(common)/user/notifications/page.tsx
+++ app/(user)/(common)/user/notifications/page.tsx
@@ -1,18 +1,30 @@
-import connectMongo from "@/lib/connectMongo";
-import NotificationsDispaly from "./_components/notifications-display";
 import { auth } from "@/auth";
+import connectMongo from "@/lib/connectMongo";
 import NotificationModel from "@/models/notifications";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import NotificationsDispaly from "./_components/notifications-display";
 
 async function NotificationsPage() {
-	await connectMongo();
-	const session = await auth();
-	const userId = session?.user.id;
+	const queryClient = new QueryClient();
+
+	await queryClient.prefetchQuery({
+		queryKey: ["notifications"],
+		queryFn: async () => {
+			await connectMongo();
+			const session = await auth();
+			const userId = session?.user.id;
+
+			const notifications = await NotificationModel.find({ _userId: userId });
+			return { notifications: JSON.parse(JSON.stringify(notifications)) };
+		},
+	});
 
 	return (
 		<div className="min-h-[100dvh] p-4 max-w-3xl mx-auto pb-10">
 			<h1 className="text-2xl font-bold mb-6">Notifications</h1>
-
-			<NotificationsDispaly />
+			<HydrationBoundary state={dehydrate(queryClient)}>
+				<NotificationsDispaly />
+			</HydrationBoundary>
 		</div>
 	);
 }
