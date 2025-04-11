5673590f787fe7bb5be0f248ccc990c769ecca71
durosly <slyboydon1@gmail.com>
2025-04-11 16:00:07 +0100

    ✅feat: update SearchBar component to include additional pathname exceptions and improve code formatting

--- app/(user)/(common)/_components/search-bar.tsx
+++ app/(user)/(common)/_components/search-bar.tsx
@@ -15,20 +15,53 @@ type PropType = {
 	isHeader?: boolean;
 };
 
-function SearchBar({ showSubmitBtn = false, direction, inputClassName, btnClassName, parentClassName, isHeader }: PropType) {
+function SearchBar({
+	showSubmitBtn = false,
+	direction,
+	inputClassName,
+	btnClassName,
+	parentClassName,
+	isHeader,
+}: PropType) {
 	const inputRef = useRef<HTMLInputElement>(null);
 	const pathname = usePathname();
 
 	if (!isHeader) {
-		const exceptions = ["/cart", "/user/checkout"];
+		const exceptions = [
+			"/cart",
+			"/user/checkout",
+			"/user",
+			"/favourites",
+			"/user/wallet",
+			"/user/orders",
+			"/user/account-details",
+		];
 		if (exceptions.includes(pathname)) return null;
 	}
 
 	return (
-		<Form action={"/browse"} className={cn("relative px-4 flex gap-2", { "flex-col": direction === "column" }, parentClassName)}>
-			<label className={cn("input relative border-[0.5px] border-[#797373] flex items-center gap-2 rounded-full", inputClassName)}>
+		<Form
+			action={"/browse"}
+			className={cn(
+				"relative px-4 flex gap-2",
+				{ "flex-col": direction === "column" },
+				parentClassName
+			)}>
+			<label
+				className={cn(
+					"input relative border-[0.5px] border-[#797373] flex items-center gap-2 rounded-full",
+					inputClassName
+				)}>
 				<IconSearchNormal className="h-5 w-5 opacity-70" />
-				<input ref={inputRef} type="search" className="grow" name="query" placeholder="Search for product" autoComplete="off" required />
+				<input
+					ref={inputRef}
+					type="search"
+					className="grow"
+					name="query"
+					placeholder="Search for product"
+					autoComplete="off"
+					required
+				/>
 				{/* suggestion dropdown */}
 				{/* <div className="absolute top-12 left-0 bg-base-100 w-full p-2 border rounded-md shadow-box z-50">
 					<ul>
@@ -51,7 +84,9 @@ function SearchBar({ showSubmitBtn = false, direction, inputClassName, btnClassN
 				</div> */}
 			</label>
 			{showSubmitBtn && (
-				<button type="submit" className={cn("btn border-none btn-neutral", btnClassName)}>
+				<button
+					type="submit"
+					className={cn("btn border-none btn-neutral", btnClassName)}>
 					Search
 				</button>
 			)}
