7d66c00b9b200f42af4edd9c2032421de84c09ac
durosly <slyboydon1@gmail.com>
2025-05-04 01:53:35 +0100

    ✅feat: enhance food item editing interface by implementing tabbed navigation for forms, improving user experience in the admin dashboard

--- app/(admin)/(dash)/dashboard/food/[foodId]/page.tsx
+++ app/(admin)/(dash)/dashboard/food/[foodId]/page.tsx
@@ -21,11 +21,48 @@ async function FoodDetailsPage({ params }: { params: Promise<{ foodId: string }>
 	return (
 		<div className="container mx-auto p-4 space-y-6">
 			<h1 className="text-3xl font-bold mb-6">Edit Food Item</h1>
-			<div className="grid grid-cols-1 lg:grid-cols-2 gap-6 ">
-				<BasicInfoForm food={foodData} />
-				<CategoryTypeForm food={foodData} />
-				<AvailabilityForm food={foodData} />
-				<ImageUploadForm food={foodData} />
+			{/* name of each tab group should be unique */}
+			<div className="tabs tabs-lift">
+				<input
+					type="radio"
+					name="my_tabs_3"
+					className="tab"
+					aria-label="Basic Info"
+					defaultChecked
+				/>
+				<div className="tab-content bg-base-100 border-base-300">
+					<BasicInfoForm food={foodData} />
+				</div>
+
+				<input
+					type="radio"
+					name="my_tabs_3"
+					className="tab"
+					aria-label="Category Type"
+				/>
+				<div className="tab-content bg-base-100 border-base-300">
+					<CategoryTypeForm food={foodData} />
+				</div>
+
+				<input
+					type="radio"
+					name="my_tabs_3"
+					className="tab"
+					aria-label="Availability"
+				/>
+				<div className="tab-content bg-base-100 border-base-300">
+					<AvailabilityForm food={foodData} />
+				</div>
+
+				<input
+					type="radio"
+					name="my_tabs_3"
+					className="tab"
+					aria-label="Image Upload"
+				/>
+				<div className="tab-content bg-base-100 border-base-300">
+					<ImageUploadForm food={foodData} />
+				</div>
 			</div>
 		</div>
 	);
