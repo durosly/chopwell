b5b6d2e2d9961b980df1b3aa0de9c3e092ff5b79
durosly <slyboydon1@gmail.com>
2025-05-18 21:07:31 +0100

    ✅feat: update DisplayUserBalance component to close cart modal on balance link click, enhancing user interaction and experience

--- app/(user)/(common)/_components/display-user-balance.tsx
+++ app/(user)/(common)/_components/display-user-balance.tsx
@@ -3,14 +3,20 @@
 import { useUserBalance } from "@/hooks/useUserData";
 import commaNumber from "@/utils/comma-number";
 import Link from "next/link";
+import useCartStore from "@/store/cart-store";
 
 function DisplayUserBalance() {
 	const { data, isLoading, isError } = useUserBalance();
+	const { closeCartModal } = useCartStore();
 
 	if (isLoading) return <span className="loading loading-spinner loading-sm"></span>;
 	if (isError) return "Error";
 
-	return <Link href="/user/wallet/top-up">{commaNumber(data?.balance)}</Link>;
+	return (
+		<Link href="/user/wallet/top-up" onClick={() => closeCartModal()}>
+			{commaNumber(data?.balance)}
+		</Link>
+	);
 }
 
 export default DisplayUserBalance;
